e9dba31f698e59f21b4a53456248fb3e9406491e
Fábio José <Fabio.Jose.t0118925@edu.atec.pt>
2024-11-04 22:54:07 +0000

    ja ta baidaway

--- resources/views/components/upload-avatar.blade.php
+++ resources/views/components/upload-avatar.blade.php
@@ -15,7 +15,7 @@
 
 <div class="flex items-center gap-4 mt-4">
     <!-- Botão de envio do formulário -->
-    <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none">
+    <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
         {{ __('Save') }}
     </button>
 
