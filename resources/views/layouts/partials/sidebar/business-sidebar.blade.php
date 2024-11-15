3732b1b887819e77ac4b3b955db2c1ec5d089fb6
Bruno Peres TPSIPL1023 <bruno.peres.t0118923@edu.atec.pt>
2024-11-15 00:48:11 +0000

    Add BusinessDetails

--- resources/views/layouts/partials/sidebar/business-sidebar.blade.php
+++ resources/views/layouts/partials/sidebar/business-sidebar.blade.php
@@ -4,5 +4,6 @@
     :links="[
         ['route' => route('business.dashboard'), 'icon' => 'fas fa-home', 'label' => 'Dashboard', 'isActive' => Request::is('business/dashboard')],
         ['route' => route('business.services.index'), 'icon' => 'fas fa-briefcase', 'label' => 'Services', 'isActive' => Route::is('business.services.index')],
+        ['route' => route('business.details.index'), 'icon' => 'fas fa-info-circle', 'label' => 'Business Details', 'isActive' => Request::is('business.details.index')],
     ]"
 />
