baae5ffa6c8cdc492899645b87988c5f2031901a
Daniil Baida <88986327+DaniilBaida@users.noreply.github.com>
2024-12-10 18:27:03 +0000

    change categories

--- database/seeders/ServiceCategorySeeder.php
+++ database/seeders/ServiceCategorySeeder.php
@@ -16,14 +16,8 @@ class ServiceCategorySeeder extends Seeder
         $categories = [
             ['name' => 'Massage Therapy', 'description' => 'Relaxing and therapeutic massages'],
             ['name' => 'Haircuts & Styling', 'description' => 'Professional haircuts and styling services'],
-            ['name' => 'Cleaning Services', 'description' => 'Residential and commercial cleaning'],
-            ['name' => 'Car Wash', 'description' => 'Comprehensive car wash and detailing'],
-            ['name' => 'Computer Repairs', 'description' => 'IT support and computer repair services'],
             ['name' => 'Math Tutoring', 'description' => 'Tutoring sessions for students of all levels'],
             ['name' => 'Photography', 'description' => 'Professional photography for events and portraits'],
-            ['name' => 'Gym Memberships', 'description' => 'Access to state-of-the-art fitness facilities'],
-            ['name' => 'Dog Walking', 'description' => 'Daily dog walking and pet care services'],
-            ['name' => 'Travel Planning', 'description' => 'Custom travel itineraries and booking services'],
         ];
 
         foreach ($categories as $category) {
